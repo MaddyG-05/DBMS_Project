c3f6c7e87eac69e636cd95e8a17690b6bbc9f51f
srijan20342 <88962475+srijan20342@users.noreply.github.com>
2022-04-25 06:13:17 +0530

    Add files via upload

--- endsem_project/admin/Admin.py
+++ endsem_project/admin/Admin.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 from click import style
 from flask import Flask,render_template, request
 from flask_mysqldb import MySQL
@@ -5,13 +6,14 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
  
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'newuser'
-app.config['MYSQL_PASSWORD'] = 'Srishti@0221'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'abcd'
 app.config['MYSQL_DB'] = 'endsem_evaluation'
 
 mysql = MySQL(app)
 
 user_id=None
+date = None
 
 t_src_id=None
 t_dest_id=None
@@ -34,187 +36,222 @@ def form():
     user_id=None
     return render_template('AdminPage.html')
 
-@app.route('/trainSchedule')
+@app.route('/Schedule')
+def Schedule():
+    return render_template('Schedule.html')
+
+@app.route('/trainS')
+def trainS():
+    global user_id
+    return render_template('trainS.html')
+
+@app.route('/trainSchedule', methods = ['POST', 'GET'])
 def trainSchedule():
-    global user_id
-    return render_template('trainSchedule.html')
+    global date
 
-@app.route('/busesSchedule')
+    if(request.method == 'GET'):
+        date = None
+        return render_template('error.html')
+
+    if(request.method == 'POST'):
+        date = request.form['dot']
+        cursor=mysql.connection.cursor()
+        query="""select distinct t.train_id, t.train_type, ts1.station_id, ts2.station_id, tfrom Train_booking where train_date ='{0}'""".format(date)
+        cursor.execute(query, ())
+        result=cursor.fetchall()
+        print(result)
+        cursor.close()
+        return render_template("trainSchedule.html", value=result)
+
+@app.route('/busesS')
+def busesS():
+    global user_id
+    return render_template('busesS.html')
+
+@app.route('/busesSchedule', methods = ['POST', 'GET'])
 def busesSchedule():
-    global user_id
-    return render_template('busesSchedule.html')
+    global date
 
-@app.route('/flightsSchedule')
-def flightsSchedule():
+    if(request.method == 'GET'):
+        date = None
+        return render_template('error.html')
+
+    if(request.method == 'POST'):
+        date = request.form['dot']
+        cursor=mysql.connection.cursor()
+        query="""select distinct bus_id from Bus_booking where bus_date ='{0}'""".format(date)
+        cursor.execute(query, ())
+        result=cursor.fetchall()
+        return render_template("busesSchedule.html", value=result)
+
+@app.route('/flightS')
+def flightS():
     global user_id
-    return render_template('flightsSchedule.html')
+    return render_template('flightS.html')
+
+@app.route('/flightsSchedule', methods = ['POST', 'GET'])
+def flightsSchedule():
+    global date
+
+    if(request.method == 'GET'):
+        date = None
+        return render_template('error.html')
+
+    if(request.method == 'POST'):
+        date = request.form['dot']
+        cursor=mysql.connection.cursor()
+        query="""select distinct flight_id from Flight_booking where flight_date ='{0}'""".format(date)
+        cursor.execute(query, ())
+        result=cursor.fetchall()
+        return render_template("flightsSchedule.html", value=result)
 
 @app.route('/Bookings')
 def Bookings():
     global user_id
     return render_template('Bookings.html')
 
-@app.route('/Prices')
-def Prices():
+@app.route('/trainB')
+def trainB():
     global user_id
-    return render_template('Prices.html')
+    return render_template('trainB.html')
 
-@app.route('/dashboard', methods=['POST', 'GET'])    #after you have registered
-def dashboard():
-    global user_id
-    if(request.method=='GET'):
-        user_id=None
+@app.route('/trainBooking', methods = ['POST', 'GET'])
+def trainBooking():
+    global date
+    global id
+
+    if(request.method == 'GET'):
+        date = None
+        id = None
         return render_template('error.html')
-        # return "Go back"
-        
-    if(request.method=='POST'):
-        id=request.form['userID']
-        password=request.form['psw']
+
+    if(request.method == 'POST'):
+        date = request.form['dot']
         cursor=mysql.connection.cursor()
-        query="""select * from User where user_id='{0}'""".format(id)
+        query="""select distinct train_id from Train_booking where train_date ='{0}'""".format(date) #write
         cursor.execute(query, ())
-        user_record=cursor.fetchone()
-        if(len(user_record)==0):
-            return render_template('wrongLogin.html')
-            # return "Wrong User ID or password"
-        if(user_record[1]!=password):
-            return render_template('wrongLogin.html')
-            # return "Wrong User ID or password"
+        result=cursor.fetchall()
+        return render_template("trainBooking.html", value=result)
 
-        user_id=id
-        cursor.close()
-        print(user_id)
-        return render_template('dashboard.html')
-
-@app.route('/postregistration', methods=['POST', 'GET'])
-def postregistration():
+@app.route('/busB')
+def busB():
     global user_id
-    if(request.method=='GET'):
-        user_id=None
-        return render_template('error.html')
-        # return "Wrong User ID or password"
+    return render_template('busB.html')
 
-    if(request.method=='POST'):
-        id=request.form['userID']
-        firstname=request.form['FirstName']
-        lastname=request.form['LastName']
-        password=request.form['psw']
-        dob=request.form['dob']
-        contactno=request.form['contactno']
-        gender=request.form['gender']
-        houseno=request.form['houseno']
-        locality=request.form['locality']
-        city=request.form['city']
-        state=request.form['state']
-        pincode=request.form['pincode']
-        # print(request.form)
-        query1="""select * from User where user_id='{0}'""".format(id)
+@app.route('/busBooking', methods = ['POST', 'GET'])
+def busBooking():
+    global date
+    global id
+
+    if(request.method == 'GET'):
+        date = None
+        id = None
+        return render_template('error.html')
+
+    if(request.method == 'POST'):
+        date = request.form['dot']
+        # id = request.form['']
         cursor=mysql.connection.cursor()
-        cursor.execute(query1, ())
-        if(len(cursor.fetchall())!=0):
-            return render_template('unsuccessReg.html')
-            # need ID main page somehow
+        query="""select distinct train_id from Train_booking where train_date ='{0}'""".format(date) #write
+        cursor.execute(query, ())
+        result=cursor.fetchall()
+        return render_template("busBooking.html", value=result)
 
-        query2="""insert into User values('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}')""".format(id, password, contactno, dob, gender, firstname, lastname, houseno, locality, city, state, pincode)
-        cursor.execute(query2, ())
-        mysql.connection.commit()
-        cursor.close()
-        return render_template('successReg.html')
-        # return "Wrong User ID or password"
-
-
-@app.route('/agentLogin', methods=['POST', 'GET'])   #after you have logged in
-def agentLogin():
-    return "agentLogin"
-
-@app.route('/contactUs', methods=['POST', 'GET'])   #selecting trains after log in
-def contactUs():
-    return render_template('contactUs.html')
-
-@app.route('/trains', methods=['POST', 'GET'])
-def trains():
+@app.route('/flightB')
+def flightB():
     global user_id
-    
-    if(user_id==None):
+    return render_template('flightB.html')
+
+@app.route('/flightBooking', methods = ['POST', 'GET'])
+def flightBooking():
+    global date
+    global id
+
+    if(request.method == 'GET'):
+        date = None
+        id = None
         return render_template('error.html')
-    return render_template('trains.html')
 
-@app.route('/train1', methods=['POST', 'GET'])
-def train1():
-    global t_src_id, t_dest_id, t_date, t_class_type, t_train_id
-    if(request.method=='GET'):
-        t_src_id=None
-        t_dest_id=None
-        t_train_id=None
-        t_class_type=None
-        t_date=None
-        return render_template('error.html')
-    # print(request.form)
-    t_src_id=request.form['from']
-    t_dest_id=request.form['to']
-    t_date=request.form['dateTravel']
-    t_class_type=request.form['class']
-    query="""
-    select t.train_id, t.train_type, ts1.departure_hour, ts1.departure_minute, ts2.arrival_hour, ts2.arrival_minute, (t.price_per_km*tdf.distance*tc.price_multiplier)
-    from Train t, train_classes tc, Train_distance_from tdf, Train_stops ts1, Train_stops ts2
-    where t.train_id=tc.train_id AND tc.class_type='{0}' AND count_of2(tc.train_id, tc.class_type, '{1}')<tc.count_of_seats AND tdf.source_id='{2}' AND tdf.destination_id='{3}' AND ts1.train_id=t.train_id AND ts2.train_id=t.train_id AND ts1.station_id='{2}' AND ts2.station_id='{3}';""".format(t_class_type, t_date, t_src_id, t_dest_id)
-    cursor=mysql.connection.cursor()
-    cursor.execute(query, ())
-    queryresult=cursor.fetchall()
-    result=[]
-    for i in queryresult:
-        temp=(i[0], i[1], str(i[2])+':'+str(i[3]), str(i[4])+':'+str(i[5]), i[6])
-        result.append(temp)
-    print(result)
-    cursor.close()
-    return render_template("train1.html",value=result)
-    
-    return "whatever"
+    if(request.method == 'POST'):
+        date = request.form['dot']
+        cursor=mysql.connection.cursor()
+        query="""select distinct train_id from Train_booking where train_date ='{0}'""".format(date) #write
+        cursor.execute(query, ())
+        result=cursor.fetchall()
+        return render_template("flightBooking.html", value=result)
 
-@app.route('/train2')
-def train2():
-    return 'train2'
-
-@app.route('/train3')
-def train3():
-    return 'train3'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@app.route('/flights', methods=['POST', 'GET'])
-def flights():
+@app.route('/Cancellation')
+def Cancellation():
     global user_id
-    
-    if(user_id==None):
-        return render_template('error.html')
-    return render_template('flights.html')
+    return render_template('Cancellation.html')
 
-@app.route('/buses', methods=['POST', 'GET'])
-def buses():
+@app.route('/trainC')
+def trainC():
     global user_id
-    print(request.method)
-    if(user_id==None):
+    return render_template('trainC.html')
+
+@app.route('/trainCancel', methods = ['POST', 'GET'])
+def trainCancel():
+    global date
+    global id
+
+    if(request.method == 'GET'):
+        date = None
+        id = None
         return render_template('error.html')
-    return render_template('buses.html')
 
-@app.route('/ticketdetails', methods=['POST', 'GET'])
-def ticketdetails():
-    print(user_id)
-    return render_template('ticketDetails.html')
+    if(request.method == 'POST'):
+        date = request.form['dot']
+        cursor=mysql.connection.cursor()
+        query="""select distinct train_id from Train_booking where train_date ='{0}'""".format(date) #write
+        cursor.execute(query, ())
+        result=cursor.fetchall()
+        return render_template("Success.html", value=result)
 
+@app.route('/busC')
+def busC():
+    global user_id
+    return render_template('busC.html')
 
+@app.route('/busCancel', methods = ['POST', 'GET'])
+def busCancel():
+    global date
+    global id
 
+    if(request.method == 'GET'):
+        date = None
+        id = None
+        return render_template('error.html')
+
+    if(request.method == 'POST'):
+        date = request.form['dot']
+        cursor=mysql.connection.cursor()
+        query="""select distinct train_id from Train_booking where train_date ='{0}'""".format(date) #write
+        cursor.execute(query, ())
+        result=cursor.fetchall()
+        return render_template("Success.html", value=result)
+
+@app.route('/flightC')
+def flightC():
+    global user_id
+    return render_template('flightC.html')
+
+@app.route('/flightCancel', methods = ['POST', 'GET'])
+def flightCancel():
+    global date
+    global id
+
+    if(request.method == 'GET'):
+        date = None
+        id = None
+        return render_template('error.html')
+
+    if(request.method == 'POST'):
+        date = request.form['dot']
+        cursor=mysql.connection.cursor()
+        query="""select distinct train_id from Train_booking where train_date ='{0}'""".format(date) #write
+        cursor.execute(query, ())
+        result=cursor.fetchall()
+        return render_template("Success.html", value=result)
 
 app.run(host='localhost', port=5000,debug=True)
--- endsem_project/flaskr/mainApp.py
+++ endsem_project/flaskr/mainApp.py
@@ -1,13 +1,13 @@
 from click import style
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, redirect
 from flask_mysqldb import MySQL
 
 
 app = Flask(__name__)
  
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'newuser'
-app.config['MYSQL_PASSWORD'] = 'Srijan123'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'abcd'
 app.config['MYSQL_DB'] = 'endsem_evaluation'
 
 mysql = MySQL(app)
@@ -19,34 +19,64 @@ t_dest_id=None
 t_train_id=None
 t_class_type=None
 t_date=None
+t_details=None
 
 b_src_id=None
 b_dest_id=None
-b_train_id=None
+b_bus_id=None
+b_details=None
 
 f_src_id=None
 f_dest_id=None
-f_train_id=None
+f_flight_id=None
 f_class_type=None
+f_details=None
+
+amount=None
+
+# common/helper URLs
+def reset():
+    global user_id, t_src_id, t_dest_id, t_train_id, t_class_type, t_date, b_src_id, b_dest_id, b_bus_id, b_details, f_src_id, f_dest_id, f_flight_id, f_class_type, f_details, amount
+    user_id=None
+
+    t_src_id=None
+    t_dest_id=None
+    t_train_id=None
+    t_class_type=None
+    t_date=None
+
+    b_src_id=None
+    b_dest_id=None
+    b_bus_id=None
+    b_details=None
+
+    f_src_id=None
+    f_dest_id=None
+    f_flight_id=None
+    f_class_type=None
+    f_details=None
+
+    amount=None
 
 @app.route('/')
 def form():
-    global user_id
-    user_id=None
+    reset()
     return render_template('mainPage.html')
+
+@app.route('/mainApp')
+def mainApp():
+    return redirect('/')
 
 @app.route('/register')
 def register():
-    global user_id
     return render_template('register.html')
 
-@app.route('/dashboard', methods=['POST', 'GET'])    #after you have registered
+@app.route('/dashboard', methods=['POST', 'GET'])    
 def dashboard():
     global user_id
     if(request.method=='GET'):
-        user_id=None
-        return render_template('error.html')
-        # return "Go back"
+        reset()
+        return redirect('/error')
         
     if(request.method=='POST'):
         id=request.form['userID']
@@ -54,15 +84,13 @@ def dashboard():
         cursor=mysql.connection.cursor()
         query="""select * from User where user_id='{0}'""".format(id)
         cursor.execute(query, ())
-        user_record=cursor.fetchone()
-        if(len(user_record)==0):
-            return render_template('wrongLogin.html')
-            # return "Wrong User ID or password"
-        if(user_record[1]!=password):
-            return render_template('wrongLogin.html')
-            # return "Wrong User ID or password"
+        user_record=cursor.fetchall()
+        print(user_record)
+        if(len(user_record)==0 or user_record==None):
+            return redirect('/wrongLogin')
+        if(user_record[0][1]!=password):
+            return redirect('/wrongLogin')
 
-        
         user_id=id
         cursor.close()
         print(user_id)
@@ -72,8 +100,8 @@ def dashboard():
 def postregistration():
     global user_id
     if(request.method=='GET'):
-        user_id=None
-        return render_template('error.html')
+        reset()
+        return redirect('/error')
         # return "Wrong User ID or password"
 
     if(request.method=='POST'):
@@ -94,7 +122,7 @@ def postregistration():
         cursor=mysql.connection.cursor()
         cursor.execute(query1, ())
         if(len(cursor.fetchall())!=0):
-            return render_template('unsuccessReg.html')
+            return redirect('/unsuccessReg')
             # need ID main page somehow
 
         query2="""insert into User values('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}')""".format(id, password, contactno, dob, gender, firstname, lastname, houseno, locality, city, state, pincode)
@@ -102,76 +130,182 @@ def postregistration():
         mysql.connection.commit()
         cursor.close()
         return render_template('successReg.html')
-        # return "Wrong User ID or password"
 
-
-@app.route('/agentLogin', methods=['POST', 'GET'])   #after you have logged in
+@app.route('/agentLogin', methods=['POST', 'GET'])   
 def agentLogin():
     return "agentLogin"
 
-@app.route('/contactUs', methods=['POST', 'GET'])   #selecting trains after log in
+@app.route('/contactUs', methods=['POST', 'GET'])   
 def contactUs():
     return render_template('contactUs.html')
 
+@app.route('/payment', methods=['POST', 'GET'])
+def payment():
+    if(request.method=='GET'):
+        reset()
+        return render_template('error.html')
+    global t_train_id, t_details
+    t_train_id=int(request.form['trainID'])
+    train_choices=[train[0] for train in t_details]
+    if(t_train_id in train_choices):
+        return render_template('paymentPortal.html')
+        
+    return redirect('/trains')
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
+
+@app.route('/wrongLogin')
+def incorrectLogin():
+    return render_template('wrongLogin.html')
+    
+@app.route('/unsuccessReg')
+def unsuccessReg():
+    return render_template('unsuccessReg.html')
+
+# ticket details
+@app.route('/ticketdetails', methods=['POST', 'GET'])
+def ticketdetails():
+    print(user_id)
+    return render_template('ticketDetails.html')
+
+@app.route('/trainTicketDetails')
+def trainTicketDetails():
+    return render_template('trainTicketDetails.html')
+
+@app.route('/busTicketDetails')
+def busTicketDetails():
+    return render_template('busTicketDetails.html')
+
+@app.route('/flightTicketDetails')
+def flightTicketDetails():
+    return render_template('flightTicketDetails.html')
+
+@app.route('/trainTicketDetails1')
+def trainTicketDetails1():
+    # return render_template('trainTicketDetails.html')
+    return "bruh"
+
+@app.route('/busTicketDetails1')
+def busTicketDetails1():
+    # return render_template('busTicketDetails.html')
+    return "bruh"
+
+@app.route('/busTicketDetails1')
+def flightTicketDetails1():
+    # return render_template('flightTicketDetails.html')
+    return "bruh"
+
+# train
 @app.route('/trains', methods=['POST', 'GET'])
 def trains():
-    global user_id
-    
+    global user_id    
     if(user_id==None):
-        return render_template('error.html')
+        return redirect('/error')
     return render_template('trains.html')
 
 @app.route('/train1', methods=['POST', 'GET'])
 def train1():
-    global t_src_id, t_dest_id, t_date, t_class_type, t_train_id
+    # something about redirecting is broken, try and fix it.
+    print(request.method)
+    global t_src_id, t_dest_id, t_date, t_class_type, t_train_id, t_details
     if(request.method=='GET'):
-        t_src_id=None
-        t_dest_id=None
-        t_train_id=None
-        t_class_type=None
-        t_date=None
-        return render_template('error.html')
+        reset()
+        return redirect('/error')
+        
     # print(request.form)
     t_src_id=request.form['from']
     t_dest_id=request.form['to']
     t_date=request.form['dateTravel']
     t_class_type=request.form['class']
     query="""
-    select t.train_id, t.train_type, ts1.departure_hour, ts1.departure_minute, ts2.arrival_hour, ts2.arrival_minute, (t.price_per_km*tdf.distance*tc.price_multiplier)
+    select t.train_id, t.train_type, ts1.departure_hour, ts1.departure_minute, ts2.arrival_hour, ts2.arrival_minute, ts2.departure_hour, ts2.departure_minute, (t.price_per_km*tdf.distance*tc.price_multiplier)
     from Train t, train_classes tc, Train_distance_from tdf, Train_stops ts1, Train_stops ts2
-    where t.train_id=tc.train_id AND tc.class_type='{0}' AND count_of2(tc.train_id, tc.class_type, '{1}')<tc.count_of_seats AND tdf.source_id='{2}' AND tdf.destination_id='{3}' AND ts1.train_id=t.train_id AND ts2.train_id=t.train_id AND ts1.station_id='{2}' AND ts2.station_id='{3}';""".format(t_class_type, t_date, t_src_id, t_dest_id)
+    where t.train_id=tc.train_id AND tc.class_type='{0}' AND count_of(tc.train_id, tc.class_type, '{1}')<tc.count_of_seats AND ((tdf.source_id='{2}' AND tdf.destination_id='{3}') OR (tdf.source_id='{3}' AND tdf.destination_id='{2}')) AND ts1.train_id=t.train_id AND ts2.train_id=t.train_id AND ts1.station_id='{2}' AND ts2.station_id='{3}' AND ((ts1.departure_day<ts2.arrival_day ) OR (ts1.departure_hour<ts2.arrival_hour) OR (ts1.departure_minute<ts2.arrival_minute));""".format(t_class_type, t_date, t_src_id, t_dest_id)
     cursor=mysql.connection.cursor()
     cursor.execute(query, ())
     queryresult=cursor.fetchall()
+    t_details=queryresult
     result=[]
     for i in queryresult:
-        temp=(i[0], i[1], str(i[2])+':'+str(i[3]), str(i[4])+':'+str(i[5]), i[6])
+        temp=(i[0], i[1], str(i[2])+':'+str(i[3]), str(i[4])+':'+str(i[5]), str(i[6])+":"+str(i[7]), i[8])
         result.append(temp)
     print(result)
     cursor.close()
+    if(len(result)==0):
+        return render_template('notAvailable.html')
     return render_template("train1.html",value=result)
-    
-    return "whatever"
 
-@app.route('/train2')
-def train2():
-    return 'train2'
+@app.route('/successBook', methods=['POST', 'GET'])
+def successBook():
+    if(request.method=='GET'):
+        return render_template('error.html')
+    global t_details, t_train_id, t_date, t_class_type, t_dest_id, t_src_id, user_id
+    global f_details, f_src_id, f_dest_id, f_
+    method=request.form['from']
+    cursor=mysql.connection.cursor()
+    query1="""
+    select max(payment_id) from Payment"""
+    cursor.execute(query1, ())
+    result=cursor.fetchall()
+    print(result)
+    print(result[0][0])
+    new_id=1
+    if(result[0][0] is None):
+        new_id=1
+    else:
+        new_id=result[0][0]+1
+    # if(len(result)!=0):
+    #     new_id=result[0][0]+1
 
-@app.route('/train3')
-def train3():
-    return 'train3'
 
 
+    if(t_src_id!=None):
+        required_train=None
+        print(t_details)
+        for train_detail in t_details:
+            if(train_detail[0]==t_train_id):
+                required_train=train_detail
+                break
+            required_train=None
+        print(t_details)
+        for train_detail in t_details:
+            if(train_detail[0]==t_train_id):
+                required_train=train_detail
+                break
+        
+        price=required_train[-1]
 
+        print(user_id)
+        query2="""
+        insert into Payment
+        values('{0}', '{1}', '{2}')""".format(new_id, price, method)
+        cursor.execute(query2, ())
+        query3="""
+        select max(ticket_id) from Train_booking"""
+        cursor.execute(query3, ())
+        new_train_id=1
+        result3=cursor.fetchall()
+        if(result3[0][0] is None):
+            new_train_id=1
+        else:
+            new_train_id=result[0][0]+1
+        mysql.connection.commit()
+        query4="""
+        insert into Train_booking
+        values('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}');""".format(t_train_id, t_src_id, t_dest_id, user_id, new_train_id, new_id, t_date, t_class_type)
+        cursor.execute(query4, ())
+        mysql.connection.commit()
+        cursor.close()
 
+    if(b_src_id!=None):
+        pass
 
+    if(f_src_id!=None):
+        pass
 
-
-
-
-
-
-
+    return render_template('successReg.html')
 
 
 
@@ -180,21 +314,10 @@ def flights():
     global user_id
     
     if(user_id==None):
-        return render_template('error.html')
+        return redirect('/error')
     return render_template('flights.html')
 
-@app.route('/buses', methods=['POST', 'GET'])
-def buses():
-    global user_id
-    print(request.method)
-    if(user_id==None):
-        return render_template('error.html')
-    return render_template('buses.html')
 
-@app.route('/ticketdetails', methods=['POST', 'GET'])
-def ticketdetails():
-    print(user_id)
-    return render_template('ticketDetails.html')
 
 
 
